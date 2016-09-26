f62b7ff83420b0459c4f26b2e4c050ac94421f50
Lefteris Karapetsas <lefteris@refu.co>
2016-09-26 13:09:39 +0200

    Add dummy defvar for flycheck

--- solidity-mode.el
+++ solidity-mode.el
@@ -384,6 +384,8 @@ Highlight the 1st result."
 
 ;;; --- interface with flycheck if existing ---
 (when (require 'flycheck nil 'noerror)
+  ;; add dummy source-inplace definition to avoid errors
+  (defvar source-inplace)
   ;; add a solidity mode callback to set the executable of solc for flycheck
   ;; define solidity's flycheck syntax checker
   (flycheck-define-checker solidity-checker
