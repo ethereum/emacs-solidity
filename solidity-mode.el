c5039e40ad8c9058b8840e35f0d0ebc125a9790b
Lefteris Karapetsas <lefteris@refu.co>
2017-12-28 16:16:29 +0100

    Add changelog and bump version

--- solidity-mode.el
+++ solidity-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Lefteris Karapetsas  <lefteris@refu.co>
 ;; Keywords: languages
-;; Version: 0.1.5
+;; Version: 0.1.6
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
